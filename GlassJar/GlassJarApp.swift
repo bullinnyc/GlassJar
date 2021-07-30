fc78b98cb38b03776853d7aafbb4b4953a9e3ccb
bullinnyc <i8u11@icloud.com>
2021-07-30 22:00:55 +0700

    Initial Commit

---
+++ GlassJar/GlassJarApp.swift
@@ -0,0 +1,18 @@
+//
+//  GlassJarApp.swift
+//  GlassJar
+//
+//  Created by Dmitry Kononchuk on 30.07.2021.
+//  Copyright © 2021 Dmitry Kononchuk. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct GlassJarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
